07bda0ededb2c7bf40634cfd34dd8db1a9b77d59
Marc <marc1890@gmail.com>
2019-02-21 18:38:29 +0100

    Revert "[2] Backoffice Categorías | https://github.com/marcvergara/cursosWebVueling/issues/2"
    This reverts commit c6c4e117eda5ae59d3d86a698a0c6d127709eae0.

--- ProyectoVueling/Controllers/CategoriasController.cs
+++ ProyectoVueling/Controllers/CategoriasController.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProyectoVueling.Models;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace ProyectoVueling.Controllers
 {
