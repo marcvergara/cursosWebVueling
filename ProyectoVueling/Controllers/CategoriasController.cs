25f6af4d1917582a16156cbc8ff856aa856602d3
ceuss <ceuss89@gmail.com>
2019-02-27 20:32:17 +0100

    Eliminados using innecesarios Movida metadata

--- ProyectoVueling/Controllers/CategoriasController.cs
+++ ProyectoVueling/Controllers/CategoriasController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProyectoVueling.Models;
 using System.IO;
-using static System.Net.Mime.MediaTypeNames;
 using Microsoft.AspNetCore.Http;
 
 namespace ProyectoVueling.Controllers
